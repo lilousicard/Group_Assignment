bc43ab9af30a45521e374c9552f920c8ae93bab2
fuelunit <yipeng.liu@sjsu.edu>
2021-07-15 21:38:38 -0700

    Initial commit by Yipeng Liu at 21:38 07/15/2021

---
+++ src/edu/sjsu/group_assignment/Main.java
@@ -0,0 +1,14 @@
+/*
+    Author  : Yipeng Liu, [Add your name], [Add your name]
+    Project : Group Assignment
+    Class   : Main
+    Date    : 07/15/2021
+ */
+package edu.sjsu.group_assignment;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
