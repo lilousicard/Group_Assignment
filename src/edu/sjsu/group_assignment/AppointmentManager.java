21a519da97467f09dbb28ba56ab872f7480b99a4
lilou <lilou.sicard-noel@sjsu.edu>
2021-07-24 19:25:36 -0700

    new class

---
+++ src/edu/sjsu/group_assignment/AppointmentManager.java
@@ -0,0 +1,65 @@
+package edu.sjsu.group_assignment;
+
+
+import java.util.*;
+
+public class AppointmentManager {
+    Map <String,Appointment> apptMap = new TreeMap<>();
+
+
+    /**
+     * add an appointment to the treemap. The appointment was already created. If the appointment already is in the
+     * treemap, the duplicate is not added
+     * @param appointment  The appointment to add.
+     * @return true or false depending on the success
+     */
+    public boolean addAppointment(Appointment appointment)
+    {
+        if (!apptMap.containsKey(appointment.getDescription()))
+        {
+            apptMap.put(appointment.getDescription(),appointment);
+            return true;
+        }
+        return false;
+
+    }
+
+    /**
+     * The method delete an appointment from the treemap using the description of the appointment to
+     * find it.
+     * @param appointment The description of the appointment to delete
+     * @return true or false depending on the success
+     */
+    public boolean deleteAppointment (String appointment)
+    {
+        if (apptMap.containsKey(appointment))
+        {
+            apptMap.remove(appointment);
+            return true;
+        }
+
+        return false;
+
+    }
+
+    /**
+     * Print all appointment in the treemap in there natural order or an error message if the treemap is empty
+     *
+     */
+    public void printAppointment()
+    {
+        if(apptMap.isEmpty())
+        {
+            System.out.println("No appointment now.");
+        }
+        else
+        {
+            for(Map.Entry<String,Appointment> entry: apptMap.entrySet())
+            {
+               System.out.println(entry.getValue().toString());
+            }
+        }
+    }
+
+
+}
---
+++ src/edu/sjsu/group_assignment/DailyAppointment.java
@@ -0,0 +1,30 @@
+package edu.sjsu.group_assignment;
+
+import java.time.LocalDate;
+
+public class DailyAppointment extends Appointment{
+
+
+    public DailyAppointment(String description, LocalDate startDate, LocalDate endDate) {
+        super(description, startDate, endDate);
+    }
+
+
+    /**+
+     * This function returns a boolean telling if there is an appointment the day that as been pass as a parameter
+     * @param date The day to find is there is an appointment
+     * @return true or false
+     */
+    @Override
+    public boolean occursOn(LocalDate date) {
+        return (isBetween(date));
+    }
+
+    /** return the type of appointment
+     * @return a string for the type
+     */
+    @Override
+    public String type() {
+        return "Daily";
+    }
+}
