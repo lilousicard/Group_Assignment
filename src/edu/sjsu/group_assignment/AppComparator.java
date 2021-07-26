49fada6aefc7fa8a2728011dbea9541a0875b2be
lilou <lilou.sicard-noel@sjsu.edu>
2021-07-26 10:17:07 -0400

    Bullet Point 1 added and works.

---
+++ src/edu/sjsu/group_assignment/AppComparator.java
@@ -0,0 +1,17 @@
+package edu.sjsu.group_assignment;
+
+import java.util.Comparator;
+
+public class AppComparator implements Comparator<Appointment> {
+
+    /** This method will compare the appointment by Start Date instead of
+     * by description
+     * @param o1 first appointment to compare
+     * @param o2 Second appointment to compare
+     * @return the Start date comparaison
+     */
+    @Override
+    public int compare(Appointment o1, Appointment o2) {
+        return o1.getStartDate().compareTo(o2.getStartDate());
+    }
+}
--- src/edu/sjsu/group_assignment/Main.java
+++ src/edu/sjsu/group_assignment/Main.java
@@ -6,9 +6,30 @@
  */
 package edu.sjsu.group_assignment;
 
+import java.time.LocalDate;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        LocalDate startDate = LocalDate.parse("2021-06-01");
+        LocalDate endDate = LocalDate.parse("2021-08-05");
+        LocalDate inBetween = LocalDate.parse("2021-07-01");
+        LocalDate before = LocalDate.parse("2021-03-05");
+        LocalDate after = LocalDate.parse("2021-10-01");
+        AppointmentManager AppMan = new AppointmentManager();
+        AppComparator compare = new AppComparator();
+
+	    Appointment daily = new DailyAppointment("Coffee",startDate,endDate);
+	    Appointment monthly = new MonthlyAppointment("Arrival",inBetween,after);
+	    Appointment oneTime = new OnetimeAppointment("Zoo", before);
+
+	    AppMan.addAppointment(daily);
+	    AppMan.addAppointment(monthly);
+	    AppMan.addAppointment(oneTime);
+
+	    AppMan.printAppointment(null);
+	    System.out.println();
+	    AppMan.printAppointment(compare);
+
     }
 }
